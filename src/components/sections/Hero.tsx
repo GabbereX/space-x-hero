b2e345d264a6a3ec468a3355bdb2468dcc1b5610
gabberex <gabberex@mail.ru>
2023-06-19 02:11:42 +0300

    fail: an attempt to implement strange elements through css failed. backup before canvas

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,13 +1,26 @@
-import { FC, ReactElement } from 'react'
+import { FC, ReactElement, useRef } from 'react'
 
 import Container from '@components/layout/Container'
 
-const Hero: FC = (): ReactElement => (
-  <section>
-    <Container className='hero'>
-      Hero
-    </Container>
-  </section>
-)
+const Hero: FC = (): ReactElement => {
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+
+  return (
+    <section className='hero'>
+      <Container className='relative'>
+        <canvas ref={ canvasRef } />
+        <div className='mars-imitation' />
+        <div className='hero-content'>
+          <h1>
+            Путешествие
+          </h1>
+          <h4>
+            на красную планету
+          </h4>
+        </div>
+      </Container>
+    </section>
+  )
+}
 
 export default Hero
